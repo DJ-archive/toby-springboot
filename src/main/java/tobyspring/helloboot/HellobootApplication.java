749b626c4231b17a8a20b9b79f7aaad1b15c2a97
DJ-archive <ydajeong7@gmail.com>
2023-03-21 21:11:31 +0900

    feat: 빈 서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,19 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// 1. 서블릿 컨테이너(톰캣) 띄우기
+		// -> 스프링부트 제공)내장형 톰캣의 초기화와 간편 설정 지원하는 TomcatServletWebServerFactory를 사용
+		// TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // jetty 등 다양한 웹 서버 지원을 위한 추상화된 ServletWebServerFactory 인터페이스
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
+
 	}
 
 }
