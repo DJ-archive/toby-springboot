b32120fe4dff846616c32641dd86539d974a2871
DJ-archive <ydajeong7@gmail.com>
2023-03-21 21:45:13 +0900

    feat: 프론트 컨트롤러

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,34 +9,38 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        // 1. 서블릿 컨테이너(톰캣) 띄우기
-        // -> 스프링부트 제공)내장형 톰캣의 초기화와 간편 설정 지원하는 TomcatServletWebServerFactory를 사용
-        // TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        // => jetty 등 다양한 웹 서버 지원을 위한 추상화된 ServletWebServerFactory 인터페이스 사용
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp)
-                    throws ServletException, IOException {
-                    // Request > 쿼리스트링 파라미터로 받은 값을 출력하는 기능을 요청
-                    // -> getParameter로 값 가져오기
-                    String name = req.getParameter("name");
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
 
-                    // Response > 웹 응답 3가지 요소 갖춰서 응답 반환
-                    resp.setStatus(HttpStatus.OK.value()); // Status Line
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Headers
-                    resp.getWriter().println("Hello " + name); // Message Body
-                    super.service(req, resp);
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 등 공통 기능 수행
+                    // 이후 요청 url에 대한 핸들러 매핑
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        // Request
+                        String name = req.getParameter("name");
+                        // Response
+                        resp.setStatus(HttpStatus.OK.value()); // Status Line
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Headers
+                        resp.getWriter().println("Hello " + name); // Message Body
+                        super.service(req, resp);
+
+                    } else if (req.getRequestURI().equals("/user")) {
+                        System.out.println("user 기능 수행");
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello"); // 매핑되는 url -> 해당 url로 요청이 들어오면 "hello" 서블릿을 수행
+            }).addMapping("/*"); // 매핑되는 url -> 해당 url로 요청이 들어오면 "hello" 서블릿을 수행
         });
         webServer.start();
 
