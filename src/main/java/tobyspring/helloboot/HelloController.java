4594ba456019ac9dfa7d018f80865b743782a4ff
DJ-archive <ydajeong7@gmail.com>
2023-04-02 17:17:07 +0900

    feat: 04 Component 스캔 - 메타 애노테이션 이용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,10 +2,9 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -14,8 +13,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
-    @ResponseBody // String으로 반환하는 값을 view 호출이 아닌, responsebody로 데이터를 넘겨주기 위해 사용 (RestController 어노테이션을 사용하면, method에 @ResponseBody를 사용하지 않아도 됨)
+    @GetMapping("/hello")
     public String hello(String name) { // 쿼리스트링으로 받을 파라미터
         // 유저의 요청사항 검증 (파라미터 체크 - Objects.requireNonNull)
         return helloService.sayHello(Objects.requireNonNull(name));
