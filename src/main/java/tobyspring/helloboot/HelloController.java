36247ef34f72c198e891292f17e96ffdf7c5e419
DJ-archive <ydajeong7@gmail.com>
2023-04-02 16:34:50 +0900

    feat: 04 DispatcherServlet + 매핑 정보 전달 (Controller내 Annotation이용)

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,9 +2,10 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -13,7 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping("/hello")
+    @GetMapping
+    @ResponseBody // String으로 반환하는 값을 view 호출이 아닌, responsebody로 데이터를 넘겨주기 위해 사용 (RestController 어노테이션을 사용하면, method에 @ResponseBody를 사용하지 않아도 됨)
     public String hello(String name) { // 쿼리스트링으로 받을 파라미터
         // 유저의 요청사항 검증 (파라미터 체크 - Objects.requireNonNull)
         return helloService.sayHello(Objects.requireNonNull(name));
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,23 +1,16 @@
 package tobyspring.helloboot;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         // 스프링 컨테이너 만들기
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class); // HelloController 클래스 정보를 넘겨 스프링 빈으로 등록해주기
         applicationContext.registerBean(SimpleHelloService.class); // SimpleHelloService도 빈으로 등록해주기
 
@@ -26,25 +19,9 @@ public class HellobootApplication {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
             // 매핑
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 등 공통 기능 수행
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        // 바인딩
-                        String name = req.getParameter("name");
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret); // Message Body
-
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*"); // 매핑되는 url -> 해당 url로 요청이 들어오면 "hello" 서블릿을 수행
+            servletContext.addServlet("dispatcherServlet",
+                new DispatcherServlet(applicationContext)
+                ).addMapping("/*"); // 매핑되는 url -> 해당 url로 요청이 들어오면 "hello" 서블릿을 수행
         });
         webServer.start();
 
