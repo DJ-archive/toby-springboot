e8151771216f8657f976345e384055b85bad12e5
DJ-archive <ydajeong7@gmail.com>
2023-04-04 21:35:48 +0900

    feat: 합성 애노테이션 적용하여 HellobootApplication 간소화

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,26 +1,9 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan // @Component가 붙은 클래스들을 빈으로 등록해준다.
+@MySpringBootApplication
 public class HellobootApplication {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
